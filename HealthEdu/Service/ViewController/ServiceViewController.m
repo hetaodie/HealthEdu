597c9b116f0e3a9830fa92c3a150c71125042808
weixu <weixu@buwangchuxindeMacBook-Pro.local>
2016-11-26 22:12:14 +0800

    完成资讯界面

--- HealthEdu/Service/ViewController/ServiceViewController.m
+++ HealthEdu/Service/ViewController/ServiceViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ServiceViewController.h"
+#import "UIColor+HEX.h"
 
-@interface ServiceViewController ()
+
+@interface ServiceViewController () <UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UIView *yuyinView;
+@property (weak, nonatomic) IBOutlet UIView *tuPianImageView;
+@property (weak, nonatomic) IBOutlet UIButton *commintButton;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -16,14 +22,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.tuPianImageView.clipsToBounds = YES;
+    self.tuPianImageView.layer.borderColor = [UIColor colorWithHexString:@"cfcece" alpha:1.0].CGColor;
+    self.tuPianImageView.layer.borderWidth = 1.0f;
+    self.tuPianImageView.layer.cornerRadius = 13;
+    
+    self.yuyinView.clipsToBounds = YES;
+    self.yuyinView.layer.borderColor = [UIColor colorWithHexString:@"cfcece" alpha:1.0].CGColor;
+    self.yuyinView.layer.borderWidth = 1.0f;
+    self.yuyinView.layer.cornerRadius = 13;
+    
+    self.commintButton.clipsToBounds = YES;
+    self.commintButton.layer.cornerRadius = 3;
+    
+    self.textView.delegate = self;
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+- (void)textViewDidBeginEditing:(UITextView *)textView{
+    if ([textView.text isEqualToString:@"请用10到500字简要描述你的问题"]) {
+        textView.text = @" ";
+    }
+}
 /*
 #pragma mark - Navigation
 
